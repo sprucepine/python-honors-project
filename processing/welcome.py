8bddc1d105e265295645c7b7a8c7a70bba849503
Davis Volpe <warning-tintype.1f@icloud.com>
2026-05-04 12:40:42 +0000

    Fix indentation for login stop message in password function

--- processing/welcome.py
+++ processing/welcome.py
@@ -2,8 +2,8 @@ def password():
     while True:
         userInput = input("Input the access code (\"q\" to quit): ")
         if userInput == "q":
-            break
             print("Login stopped. You must restart the program in order to continue.")
+            break
         elif userInput == "1234":
             print("Access granted.")
             break
